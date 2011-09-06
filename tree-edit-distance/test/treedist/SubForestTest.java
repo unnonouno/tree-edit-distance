4c22145c5826f8038c4cbc9c351544055447f74f
Yuya Unno <unnonouno@gmail.com>
2011-09-06 22:31:04 +0900

    made unit test for SubForest

--- tree-edit-distance/test/treedist/SubForestTest.java
+++ tree-edit-distance/test/treedist/SubForestTest.java
@@ -8,8 +8,8 @@ public class SubForestTest {
 	@Test
 	public void trivial() {
 		Tree t = new LabeledTree( //
-				new int[]{-1, 0, 0}, //
-				new int[]{0, 1, 2});
+				new int[] { -1, 0, 0 }, //
+				new int[] { 0, 1, 2 });
 		SubForest f = new SubForest(t);
 		Assert.assertEquals(0, f.deleteHead().root());
 		Assert.assertEquals(1, f.deleteHead().head());
@@ -18,5 +18,27 @@ public class SubForestTest {
 		Assert.assertEquals(1, f.getHeadChild().head());
 
 		Assert.assertNull(f.getSibling());
+	}
+
+	@Test
+	public void equalTest() {
+		Tree t = new LabeledTree( //
+				new int[] { -1, 0, 0 }, //
+				new int[] { 0, 1, 2 });
+
+		SubForest root = new SubForest(t);
+		Assert.assertEquals(root, new SubForest(t));
+		Assert.assertEquals(root.hashCode(), new SubForest(t).hashCode());
+
+		Assert.assertEquals(root.getHeadChild(), root.getHeadChild());
+		Assert.assertEquals(root.getHeadChild().hashCode(), root.getHeadChild()
+				.hashCode());
+
+		// null
+		Assert.assertEquals(root.getSibling(), root.getSibling());
+
+		Assert.assertEquals(root.deleteHead(), root.deleteHead());
+		Assert.assertEquals(root.deleteHead().hashCode(), root.deleteHead()
+				.hashCode());
 	}
 }
