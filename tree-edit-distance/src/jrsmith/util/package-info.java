195f93c9b7a25d57a00344f0c4f076fa5d1456f7
Joshua Smith <jsmit272@jhu.edu>
2012-01-16 17:05:56 -0500

    Formatting

--- tree-edit-distance/src/jrsmith/util/package-info.java
+++ tree-edit-distance/src/jrsmith/util/package-info.java
@@ -1,4 +1,5 @@
 /**
- * A collection of useful reusable classes that are not related to any particular application. 
+ * A collection of useful reusable classes that are not related to 
+ * any particular application. 
  */
 package jrsmith.util;
