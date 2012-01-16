ef497c35a1eb65e819ec4304e2746315d92c46e8
Joshua Smith <jsmit272@jhu.edu>
2012-01-16 17:07:31 -0500

    Initial Commit of generic Package
    The generic package contains Contains an interface and implementation
    for a generic tree. A generic tree mirrors a tree, but allows for
    generic values at each node rather than simply integers. Generic trees
    can be interacted with using the values contained in the tree, the
    integer IDs for these values, or a combination of both.
    
    This commit includes implementations and comments.

---
+++ tree-edit-distance/src/treedist/generic/AbstractGenericTree.java
@@ -0,0 +1,214 @@
+/**
+ * 
+ */
+package treedist.generic;
+
+import treedist.Tree;
+
+/**
+ * Provides a partial implementation of the GenericTree interface. Classes that
+ * inherit from AbstractGenericTree only need to implement and underlying tree
+ * of integers and a mapping between these integers and values. They implement
+ * these through the methods: getTree, getFoundNode, and getId. All other
+ * methods in the GenericTree interface are implemented from these.
+ * 
+ * @author Joshua Smith
+ * 
+ * @param <T>
+ *            The type of the values in the GenericTree.
+ * 
+ */
+public abstract class AbstractGenericTree<T> implements GenericTree<T> {
+
+	/**
+	 * Get the value in the node at nodeId in the tree. Behavior need not be
+	 * defined if nodeId is not the ID of a node in the tree.
+	 * 
+	 * @param nodeId
+	 *            the ID of the node to find.
+	 * @return the value at the node with ID nodeId.
+	 */
+	protected abstract T getFoundNode(int nodeId);
+
+	/**
+	 * Get the underlying tree implementation of this abstract tree. The
+	 * integers in this tree will be used as the nodeId's for the abstract
+	 * generic tree.
+	 * 
+	 * @return the underlying tree.
+	 */
+	protected abstract Tree getTree();
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.generic.GenericTree#getId(T)
+	 */
+	@Override
+	public abstract int getId(T value);
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.generic.GenericTree#getNode(int)
+	 */
+	@Override
+	public final T getNode(final int nodeId) {
+		if (nodeId == Tree.NOT_FOUND) {
+			return null;
+		} else {
+			return getFoundNode(nodeId);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.Tree#getRoot()
+	 */
+	@Override
+	public final int getRoot() {
+		return getTree().getRoot();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.Tree#getFirstChild(int)
+	 */
+	@Override
+	public final int getFirstChild(final int nodeId) {
+		return getTree().getFirstChild(nodeId);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.Tree#getNextSibling(int)
+	 */
+	@Override
+	public final int getNextSibling(final int nodeId) {
+		return getTree().getNextSibling(nodeId);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.Tree#getParent(int)
+	 */
+	@Override
+	public final int getParent(final int nodeId) {
+		return getTree().getParent(nodeId);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.Tree#size()
+	 */
+	@Override
+	public final int size() {
+		return getTree().size();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.generic.GenericTree#getFirstChild(java.lang.Object)
+	 */
+	@Override
+	public final int getFirstChild(final T node) {
+		return getFirstChild(getId(node));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.generic.GenericTree#getNextSibling(java.lang.Object)
+	 */
+	@Override
+	public final int getNextSibling(final T node) {
+		return getNextSibling(getId(node));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.generic.GenericTree#getParent(java.lang.Object)
+	 */
+	@Override
+	public final int getParent(final T node) {
+		return getParent(getId(node));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.generic.GenericTree#getRootNode()
+	 */
+	@Override
+	public final T getRootNode() {
+		return getNode(getRoot());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.generic.GenericTree#getFirstChildNode(int)
+	 */
+	@Override
+	public final T getFirstChildNode(final int nodeId) {
+		return getNode(getFirstChild(nodeId));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.generic.GenericTree#getFirstChildNode(java.lang.Object)
+	 */
+	@Override
+	public final T getFirstChildNode(final T node) {
+		return getNode(getFirstChild(node));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.generic.GenericTree#getNextSiblingNode(int)
+	 */
+	@Override
+	public final T getNextSiblingNode(final int nodeId) {
+		return getNode(getNextSibling(nodeId));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.generic.GenericTree#getNextSiblingNode(java.lang.Object)
+	 */
+	@Override
+	public final T getNextSiblingNode(final T node) {
+		return getNode(getNextSibling(node));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.generic.GenericTree#getParentNode(int)
+	 */
+	@Override
+	public final T getParentNode(final int nodeId) {
+		return getNode(getParent(nodeId));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.generic.GenericTree#getParentNode(java.lang.Object)
+	 */
+	@Override
+	public final T getParentNode(final T node) {
+		return getNode(getParent(node));
+	}
+
+}
---
+++ tree-edit-distance/src/treedist/generic/GenericTreeImpl.java
@@ -0,0 +1,125 @@
+/**
+ * 
+ */
+package treedist.generic;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import jrsmith.util.LookupIdStore;
+import treedist.Tree;
+import treedist.TreeImpl;
+
+/**
+ * Simple implementation of the GenericTree interface using a TreeImpl for the
+ * underlying tree. This tree is immutable once it has been created.
+ * 
+ * @author Joshua Smith
+ * 
+ * @param <T>
+ *            The type of the values in the GenericTree.
+ */
+public class GenericTreeImpl<T> extends AbstractGenericTree<T> {
+
+	/**
+	 * A map between values and IDs.
+	 */
+	private final LookupIdStore<T> ids;
+
+	/**
+	 * The underlying tree of IDs.
+	 */
+	private final TreeImpl tree;
+
+	/**
+	 * Creates a tree containing the values in nodes. Parents contains the
+	 * values for the parents of the values in nodes, such that
+	 * getParentNode(nodes[i]) == parents[i].
+	 * 
+	 * All values in parents must equal a value in nodes, with the exception of
+	 * one which must be null. The node with the index of the null value in
+	 * parents is the root of the tree.
+	 * 
+	 * @param nodes
+	 *            the values in the tree.
+	 * @param parents
+	 *            the values of the parent of each node in nodes as specified
+	 *            above.
+	 */
+	public GenericTreeImpl(final List<T> nodes, final List<T> parents) {
+		if (nodes.size() != parents.size()) {
+			throw new IllegalArgumentException(
+					"Parents list must be the same size os Nodes list.");
+		}
+		this.ids = new LookupIdStore<T>(nodes);
+
+		int[] parentArray = makeIdArray(parents, ids);
+		this.tree = new TreeImpl(parentArray);
+	}
+
+	/**
+	 * Creates an array of the IDs in values, given a LookupIdStore that maps
+	 * values to IDs. The resulting array is ordered such that each ID is at the
+	 * same index as its corresponding value in values. All values not contained
+	 * in ids, are given the ID: Tree.NOT_FOUND.
+	 * 
+	 * @param values
+	 *            a list of values
+	 * @param ids
+	 *            a map from values to IDs.
+	 * @return an array of the IDs of each value in values.
+	 * 
+	 * @param <T>
+	 *            the type of the values to map to IDs.
+	 */
+	private static <T> int[] makeIdArray(final List<T> values,
+			final LookupIdStore<T> ids) {
+		int[] idArray = new int[values.size()];
+
+		int length = values.size();
+		for (int index = 0; index < length; index++) {
+			T value = values.get(index);
+			if (value == null) {
+				idArray[index] = Tree.NOT_FOUND;
+			} else {
+				idArray[index] = ids.getId(value);
+			}
+		}
+
+		return idArray;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.generic.AbstractGenericTree#getId(java.lang.Object)
+	 */
+	@Override
+	public final int getId(final T node) {
+		try {
+			return ids.getId(node);
+		} catch (NoSuchElementException e) {
+			return Tree.NOT_FOUND;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.generic.AbstractGenericTree#getFoundNode(int)
+	 */
+	@Override
+	protected final T getFoundNode(final int nodeId) {
+		return ids.getItem(nodeId);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see treedist.generic.AbstractGenericTree#getTree()
+	 */
+	@Override
+	protected final Tree getTree() {
+		return tree;
+	}
+}
